a3d0ef1725dda4d0fa1847da50b8b1fe48026937
kokochef <kakoty.kaustabh@gmail.com>
2018-10-28 17:56:06 +0530

    api works with predictions

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 import numpy as np 
+import tensorflow as tf 
 from keras.models import load_model
 from flask import Flask, jsonify, render_template, request
 from PIL import Image
 
 def loadModel():
-    global model
+    global model,graph
     model = load_model('/predictor.h5')
+    graph = tf.get_default_graph()
 
 app = Flask(__name__)
 
 @app.route('/api/mnist', methods = ['POST'])
 def mnist():
-    img = Image.open(request.files['image'])
-    print(img.shape)    
-    return jsonify(result="success")
+    img = np.array(Image.open(request.files['image']))
+    img = img.reshape((28, 28, 1)).astype('float32')
+    img /=255.0
+    with graph.as_default():
+        return jsonify(results=model.predict(np.expand_dims(img, axis=0)).tolist())
+
 
 if __name__ == '__main__':
     loadModel()
